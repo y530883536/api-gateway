db3deddc85b70d507910f3eb67f5091bdb12db7d
ycf <530883536@qq.com>
2021-05-21 14:14:27 +0800

    回到原点

--- shop-commom/src/main/java/com/ycf/model/Product.java
+++ shop-commom/src/main/java/com/ycf/model/Product.java
@@ -7,19 +7,4 @@ public class Product {
     private Integer id;
     private String name;
     private Double price;
-
-    //这个方法是做分支切换测试的，没有意义，testYcf
-    public void testYcf1() {
-
-    }
-
-    //这个方法是做分支切换测试的，没有意义，testYcf
-    public void testYcf2() {
-
-    }
-
-    //这个方法是做分支切换测试的，没有意义，testYcf
-    public void testYcf3() {
-
-    }
 }
