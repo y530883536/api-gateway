1621dbabf11c6bb8a0d3f4554410bfea0baa43d7
ycf <530883536@qq.com>
2021-05-21 14:24:40 +0800

    回到原点

--- shop-commom/src/main/java/com/ycf/model/Product.java
+++ shop-commom/src/main/java/com/ycf/model/Product.java
@@ -2,25 +2,9 @@ package com.ycf.model;
 
 import lombok.Data;
 
-//新分支修改一个文件
 @Data
 public class Product {
     private Integer id;
     private String name;
     private Double price;
-
-    //这个方法是做分支切换测试的，没有意义，testMaster
-    public void testMaster1() {
-
-    }
-
-    //这个方法是做分支切换测试的，没有意义，testMaster
-    public void testMaster2() {
-
-    }
-
-    //这个方法是做分支切换测试的，没有意义，testMaster
-    public void testMaster3() {
-
-    }
 }
